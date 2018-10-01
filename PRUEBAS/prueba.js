96fa35bd1fb3bc7f7100a720e5ef90eff0f994f9
Matias Jara <matiasjara@users.noreply.github.com>
2018-10-01 15:16:50 -0300

    new
    new

---
+++ PRUEBAS/prueba.js
@@ -0,0 +1,7 @@
+var numIngresado = prompt("Ingresar un Numero");
+var valIngresado = parseInt (numIngresado);
+if(valIngresado % 2 === 0){
+  alert(El numero ingresado es par);
+}else {
+  alert(El numero ingresado es impar)
+}
